e55982bb90d6a944adf508331e0899eaabdd90fd
Mohamed Amine Bennacef <68336176+mohamedaminebennacef@users.noreply.github.com>
2025-02-05 14:11:00 +0100

    Lists and Keys(Rendering Lists)

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,15 +1,17 @@
 const ListGroup = () => {
+  const items = ["An item", "A second item", "A third item", "A fourth item"];
+
   return (
-   <>
-    <h1>List</h1>
-    <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
-   </>
+    <>
+      <h1>List</h1>
+      <ul className="list-group">
+        {items.map((item) => (
+          <li className="list-group-item" key={item}>
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 
