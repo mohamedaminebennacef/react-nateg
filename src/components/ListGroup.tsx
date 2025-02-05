6b72f09e44e3c464f38b72eae591772adffe27f7
Mohamed Amine Bennacef <68336176+mohamedaminebennacef@users.noreply.github.com>
2025-02-05 14:52:47 +0100

    Events : onClick={(event:MouseEvent)}

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,21 +1,18 @@
+import { MouseEvent } from "react";
 const ListGroup = () => {
-  const items:string[] = ["item1", "item2", "item3", "item4"];
-  // if (items.length === 0) {
-  //   return (
-  //     <>
-  //       <h1>List</h1>
-  //       <p>No items Found</p>
-  //     </>
-  //   );
-  // }
+  const items: string[] = ["item1", "item2", "item3", "item4"];
+
+  // Event handler
+  const handleClick = (event: MouseEvent) => console.log(event);
+
   return (
     <>
       <h1>List</h1>
-      {/* {items.length === 0 ? <p>No items Found</p>:null} */}
       {items.length === 0 && <p>No items Found</p>}
       <ul className="list-group">
-        {items.map((item:string) => (
-          <li className="list-group-item" key={item}>
+        {items.map((item: string) => (
+          <li className="list-group-item" key={item} onClick={handleClick}>
+            {/*refrence not calling the function*/}
             {item}
           </li>
         ))}
