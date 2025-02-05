4285c99379c042035c5bac419a2621e469920959
Mohamed Amine Bennacef <68336176+mohamedaminebennacef@users.noreply.github.com>
2025-02-05 12:44:16 +0100

    Fragments

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,5 +1,7 @@
 const ListGroup = () => {
   return (
+   <>
+    <h1>List</h1>
     <ul className="list-group">
       <li className="list-group-item">An item</li>
       <li className="list-group-item">A second item</li>
@@ -7,6 +9,7 @@ const ListGroup = () => {
       <li className="list-group-item">A fourth item</li>
       <li className="list-group-item">And a fifth one</li>
     </ul>
+   </>
   );
 };
 
